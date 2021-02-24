f00cc186fc2a4bab364379196e81751d02383819
ahmetsyildirim <ahmets.yildirim@gmail.com>
2021-02-24 14:01:33 +1100

    initial commit

---
+++ HotelApi/HotelApi/Controllers/HotelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HotelApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelController : ControllerBase
+    {
+
+        private readonly ILogger<HotelController> _logger;
+
+        public HotelController(ILogger<HotelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Hotel> Get()
+        {
+            var hotelList = new List<Hotel>() { new Hotel { Name = "Beach Hotel", Address = "Beach Side", Description = "Surf Hotel", Score = 4 }, new Hotel { Name = "Castle Hotel", Address = "Mountain Road", Description = "Historic Hotel", Score = 5 } };
+
+            return hotelList;
+        }
+    }
+}
---
+++ HotelApi/HotelApi/Hotel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HotelApi
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+    }
+}
